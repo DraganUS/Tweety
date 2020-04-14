d460fb578aa9ac2856d3c39db9910db109e4e189
DraganUS <usic.d.dragan@gmail.com>
2020-04-14 18:51:15 +0200

    adding tweet -fms

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="lg:flex">
-        <div class="lg:w-1/6">
+    <div class="lg:flex lg:justify-between">
+        <div class="lg:w-32">
           @include('_sidebar-links')
         </div>
 
-        <div class="lg:flex-1 lg:mx-10">
+        <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
             @include('_publish-tweet-panel')
-
             <div class="border border-gray-300 rounded-lg">
-                <div class="flex p-4">
-                    <div class="mr-2 flex-shrink-0">
-                        <img
-                            src="https://i.pravatar.cc/50"
-                            alt=""
-                            class="rounded-full mr-2"
-                        >
-                    </div>
-
-                    <div>
-                        <h5 class="font-bold mb-4">John Doe</h5>
-
-                        <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto deserunt fugiat labore neque quisquam temporibus voluptas. Beatae dicta ea error eveniet excepturi fuga iusto laboriosam laudantium perferendis placeat, sapiente voluptates. Aut dolorem eveniet explicabo necessitatibus neque non officiis placeat reiciendis tempora vero. Accusantium amet architecto assumenda at consequatur dicta dolorem enim eum exercitationem harum illo inventore, ipsa, laudantium maiores minima obcaecati officiis perferendis quam quas quod saepe suscipit tenetur, totam velit voluptatem. Consequatur dolore laboriosam qui repellat temporibus. Asperiores enim explicabo laudantium quam. Animi aperiam, consectetur consequatur delectus et facere hic incidunt iusto perferendis quasi quibusdam rerum saepe, suscipit.</p>
-                    </div>
-                </div>
+                @foreach($tweets as $tweet)
+                    @include('_tweet')
+                @endforeach
             </div>
-
         </div>
 
-        <div class="lg:w-1/6 ">
+        <div class="lg:w-1/6 bg-blue-100 rounded-lg p-4">
             @include('_friends-list')
         </div>
     </div>
