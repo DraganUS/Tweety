e3aa376fe08266bdcd115cbfe858253ee5c0572a
DraganUS <usic.d.dragan@gmail.com>
2020-04-14 18:11:26 +0200

    adding lauout

---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,10 @@
+<ul>
+    <li><a href="" class="font-bold text lg mb-4 blok">Home</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">Explore</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">Notifications</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">Message</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">Bookmarks</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">Lists</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">Profile</a></li>
+    <li><a href="" class="font-bold text lg mb-4 blok">More</a></li>
+</ul>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="lg:flex">
+        <div class="lg:w-1/6">
+          @include('_sidebar-links')
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="lg:flex-1 lg:mx-10">
+            @include('_publish-tweet-panel')
 
-                    You are logged in!
+            <div class="border border-gray-300 rounded-lg">
+                <div class="flex p-4">
+                    <div class="mr-2 flex-shrink-0">
+                        <img
+                            src="https://i.pravatar.cc/50"
+                            alt=""
+                            class="rounded-full mr-2"
+                        >
+                    </div>
+
+                    <div>
+                        <h5 class="font-bold mb-4">John Doe</h5>
+
+                        <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto deserunt fugiat labore neque quisquam temporibus voluptas. Beatae dicta ea error eveniet excepturi fuga iusto laboriosam laudantium perferendis placeat, sapiente voluptates. Aut dolorem eveniet explicabo necessitatibus neque non officiis placeat reiciendis tempora vero. Accusantium amet architecto assumenda at consequatur dicta dolorem enim eum exercitationem harum illo inventore, ipsa, laudantium maiores minima obcaecati officiis perferendis quam quas quod saepe suscipit tenetur, totam velit voluptatem. Consequatur dolore laboriosam qui repellat temporibus. Asperiores enim explicabo laudantium quam. Animi aperiam, consectetur consequatur delectus et facere hic incidunt iusto perferendis quasi quibusdam rerum saepe, suscipit.</p>
+                    </div>
                 </div>
             </div>
+
+        </div>
+
+        <div class="lg:w-1/6 ">
+            @include('_friends-list')
         </div>
     </div>
-</div>
 @endsection
