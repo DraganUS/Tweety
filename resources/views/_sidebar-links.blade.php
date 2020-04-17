12c980190aa8cd1746505bb98d752efd7d333e62
DraganUS <usic.d.dragan@gmail.com>
2020-04-17 14:21:24 +0200

    some impruvments

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -1,10 +1,11 @@
 <ul>
     <li><a href="{{ route('home') }}" class="font-bold text lg mb-4 blok">Home</a></li>
     <li><a href="/explore" class="font-bold text lg mb-4 blok">Explore</a></li>
-    <li><a href="" class="font-bold text lg mb-4 blok">Notifications</a></li>
-    <li><a href="" class="font-bold text lg mb-4 blok">Message</a></li>
-    <li><a href="" class="font-bold text lg mb-4 blok">Bookmarks</a></li>
-    <li><a href="" class="font-bold text lg mb-4 blok">Lists</a></li>
     <li><a href="{{ route('profile', auth()->user()) }}" class="font-bold text lg mb-4 blok">Profile</a></li>
-    <li><a href="" class="font-bold text lg mb-4 blok">More</a></li>
+    <li>
+        <form method="POST" action="/logout">
+            @csrf
+            <button class="font-bold text lg mb-4 blok" >Logout</button>
+        </form>
+    </li>
 </ul>
